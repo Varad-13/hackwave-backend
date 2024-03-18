dd4c2686424db1654057da177249b649920377e7
Meet Jamsutkar <jamsutkarmeetpradeep@gmail.com>
2024-03-18 14:00:10 +0530

    Add team Model & added django-import-export

--- core/models.py
+++ core/models.py
@@ -17,10 +17,26 @@ class User(AbstractUser):
     college = models.ForeignKey(College, on_delete=models.CASCADE)
     year = models.CharField(max_length= 15, default="Year")
     dept = models.CharField(max_length=20, default="Department" )
+    activated = models.BooleanField(default=False)
 
 
     USERNAME_FIELD = 'email'
 
     REQUIRED_FIELDS = ['username']
+
+
+
+DOMAINS = (
+     ('Sustainibility', 'Sustainibility'),
+     ('Intelligent Automation', 'Intelligent Automation')
+
+)
+class Team(models.Model):
+    name = models.CharField(max_length=500, null=True)
+    lead = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lead')
+    mem2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='mem2', null=True)
+    mem3 = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='mem3')
+    mem3 = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='mem4')
+    domain = models.CharField(max_length=500, choices = DOMAINS)
 
 # Create your models here.
